1f8fd95223c91ca81a146c83b7768a3dea595578
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2025-03-30 15:02:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './Redux/Store';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from "react-redux";
+import store from './utils/store';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router basename='youtube-clone'>
-        <App />
-      </Router>
+    <Provider store = {store}>
+      <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
